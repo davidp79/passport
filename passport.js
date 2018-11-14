4a156f533e6630ea598d9a072de9f34b11a0bd83
davidp79 <freetoflow442@yahoo.com>
2018-11-14 18:38:33 -0500

    changes

--- passport.js
+++ passport.js
@@ -1,11 +1,11 @@
-var bCrypt = require('bcrypt-nodejs');
+const bCrypt = require('bcrypt-nodejs');
  
  
 module.exports = function(passport, user) {
  
-    var User = user;
+    const User = user;
  
-    var LocalStrategy = require('passport-local').Strategy;
+     const LocalStrategy = require('passport-local').Strategy;
  
     passport.use('local-signup', new LocalStrategy(
  
@@ -21,7 +21,7 @@ module.exports = function(passport, user) {
  
         function(req, email, password, done) {
  
-            var generateHash = function(password) {
+            const generateHash = function(password) {
  
                 return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
  
@@ -45,9 +45,9 @@ module.exports = function(passport, user) {
  
                 {
  
-                    var userPassword = generateHash(password);
+                    let userPassword = generateHash(password);
  
-                    var data =
+                    let data =
  
                         {
                             email: email,
@@ -115,9 +115,9 @@ passport.use('local-signin', new LocalStrategy(
  
     function(req, email, password, done) {
  
-        var User = user;
+        let User = user;
  
-        var isValidPassword = function(userpass, password) {
+        const isValidPassword = function(userpass, password) {
  
             return bCrypt.compareSync(password, userpass);
  
@@ -143,5 +143,5 @@ passport.use('local-signin', new LocalStrategy(
                     message: 'Incorrect password.'
                 });
  
-            }
+            };
  
